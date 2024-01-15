fd5b5e6e48ad070a07b163594414a3351cd5a5d8
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-15 16:05:50 +0330

    Add custom css and font and bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/global/custom.css";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
