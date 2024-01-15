cb6310b739acc0e6f94476ec113a3c0ef0ea21f6
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-15 16:10:01 +0330

    Add header module to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/global/custom.css";
+import Header from "@/components/modules/Header/Header";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
