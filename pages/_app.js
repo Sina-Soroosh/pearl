ad5ce2f63b839624a0e17bbcbf5b2a1a8d786a37
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-15 16:16:16 +0330

    Add logo to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/global/custom.css";
 import Header from "@/components/modules/Header/Header";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/images/logo/pearl-favicon-color.png"
+        />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
