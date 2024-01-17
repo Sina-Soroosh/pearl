4bc5c31d4cf8514ae5386aebda34c07d2d5f2476
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-17 19:10:56 +0330

    Coding & completed CategoryCard module & completed Categories template

---
+++ components/modules/CategoryCart/CategoryCart.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "@/styles/modules/CategoryCart/CategoryCart.module.css";
+import Link from "next/link";
+
+function CategoryCart() {
+  return (
+    <>
+      <div className="col-lg-3 col-md-4 .col-sm-6">
+        <div className={styles.category_cart}>
+          <div className={styles.content}>
+            <div className={styles.title}>
+              <Link href="/">
+                <h2>صندلی</h2>
+              </Link>
+            </div>
+            <div className={styles.desc}>
+              <span>4 مورد</span>
+            </div>
+            <div className={styles.link}>
+              <Link href="/">+ نمایش مجموعه</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CategoryCart;
--- components/templates/Home/Categories.js
+++ components/templates/Home/Categories.js
@@ -1,10 +1,19 @@
 import React from "react";
 import styles from "@/styles/templates/Home/Categories.module.css";
+import CategoryCart from "@/components/modules/CategoryCart/CategoryCart";
 
 function Categories() {
   return (
     <>
-      <div className={styles.categories}></div>
+      <div className={styles.categories}>
+        <div className="row">
+          <CategoryCart />
+          <CategoryCart />
+          <CategoryCart />
+          <CategoryCart />
+          <CategoryCart />
+        </div>
+      </div>
     </>
   );
 }
