64e59cce7d902e26660a915b7a13be9f1adb2e34
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-20 17:42:14 +0330

    Complete checkout page

--- components/templates/Checkout/Main.js
+++ components/templates/Checkout/Main.js
@@ -18,7 +18,35 @@ function Main() {
               <div className="col-lg-8">
                 <CheckoutForm />
               </div>
-              <div className="col-lg-4"></div>
+              <div className="col-lg-4">
+                <div className={styles.list}>
+                  <div className={styles.title}>
+                    <h3>سفارش شما</h3>
+                  </div>
+                  <ul className={styles.items}>
+                    <li>
+                      <div className={styles.name}>
+                        <span>اسپیکر × 2</span>
+                      </div>
+                      <div className={styles.price}>
+                        <span>144,000 تومان</span>
+                      </div>
+                    </li>
+                    <li>
+                      <div className={styles.name}>
+                        <span>اسپیکر × 2</span>
+                      </div>
+                      <div className={styles.price}>
+                        <span>144,000 تومان</span>
+                      </div>
+                    </li>
+                  </ul>
+                  <div className={styles.total}>
+                    <span>مجموع</span>
+                    <span className={styles.price}>220,000 تومان</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
