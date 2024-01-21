c281794a1cc937158ce4ece6f93a1685b9fe53ba
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-21 16:04:54 +0330

    Coding warranty-and-services page

---
+++ pages/warranty-and-services/index.js
@@ -0,0 +1,17 @@
+import Main from "@/components/templates/WarrantyAndServices/Main";
+import Head from "next/head";
+import React from "react";
+
+function WarrantyAndServices() {
+  return (
+    <>
+      <Head>
+        <title>Pearl - گارانتی و خدمات</title>
+      </Head>
+
+      <Main />
+    </>
+  );
+}
+
+export default WarrantyAndServices;
