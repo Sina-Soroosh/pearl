dcf42921921aa9287f295123026af1c3be3a3610
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-22 13:16:04 +0330

    Complete contact-us page

--- components/templates/ContactUs/Main.js
+++ components/templates/ContactUs/Main.js
@@ -23,6 +23,14 @@ function Main() {
               <div className={styles.form}>
                 <ContactUsForm />
               </div>
+              <div className={styles.map}>
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d38168.32903282598!2d53.55991384020386!3d28.50716986022733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1705916390330!5m2!1sen!2s"
+                  allowfullscreen=""
+                  loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </div>
             </div>
           </div>
         </div>
