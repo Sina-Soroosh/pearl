dfe5c8aebfa53e65cc4716f629f8accf8ba87283
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-23 19:06:45 +0330

    Coding  my-account/edit-account page

---
+++ components/templates/MyAccount/EditAccount/Main.js
@@ -0,0 +1,37 @@
+import Breadcrumb from "@/components/modules/Breadcrumb/Breadcrumb";
+import MyAccountMenu from "@/components/modules/MyAccountMenu/MyAccountMenu";
+import React from "react";
+import styles from "@/styles/templates/MyAccount/EditAccount/Main.module.css";
+
+function Main() {
+  return (
+    <>
+      <Breadcrumb
+        links={[
+          { id: 1, title: "حساب کاربری من", href: "/my-account" },
+          {
+            id: 2,
+            title: "جزییات حساب کاربری",
+            href: "/my-account/edit-account",
+          },
+        ]}
+        title="جزییات حساب کاربری"
+      />
+
+      <div className={styles.account}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <MyAccountMenu />
+            </div>
+            <div className="col-md-9">
+              <div className={styles.content}></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Main;
---
+++ pages/my-account/edit-account/index.js
@@ -0,0 +1,17 @@
+import Main from "@/components/templates/MyAccount/EditAccount/Main";
+import Head from "next/head";
+import React from "react";
+
+function EditAccount() {
+  return (
+    <>
+      <Head>
+        <title>Pearl - جزییات حساب کاربری</title>
+      </Head>
+
+      <Main />
+    </>
+  );
+}
+
+export default EditAccount;
