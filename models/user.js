3ea3e01a78e156f451814ce6e17d68df12383ee8
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-24 09:29:35 +0330

    Coding user model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const { default: mongoose } = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    minLength: 3,
+    required: true,
+    index: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    minLength: 3,
+    required: true,
+    index: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["ADMIN", "USER"],
+    default: "USER",
+    required: true,
+  },
+});
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
