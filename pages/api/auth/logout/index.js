f79d886fadcd47a633bc88634fd7814abe772bc9
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-24 11:09:59 +0330

    Coding logout api

---
+++ pages/api/auth/logout/index.js
@@ -0,0 +1,27 @@
+import { connectToDB } from "@/config/db";
+import { serialize } from "cookie";
+
+const logout = async (req, res) => {
+  connectToDB();
+
+  try {
+    switch (req.method) {
+      case "GET":
+        const cookie = serialize("user_token", "", {
+          httpOnly: true,
+          path: "/",
+          maxAge: -1,
+        });
+
+        return res
+          .setHeader("Set-Cookie", cookie)
+          .json({ message: "Logout successfully :))" });
+      default:
+        return res.status(405).json({ message: "The method is not valid" });
+    }
+  } catch (error) {
+    return res.status(500).res({ message: "Unknown internal server error !!" });
+  }
+};
+
+export default logout;
