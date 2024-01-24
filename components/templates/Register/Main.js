11c17e61f5980ef6267b8e39288d1068da53bcfd
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-24 12:34:58 +0330

    Connecting the register page with the backend

--- components/templates/Register/Main.js
+++ components/templates/Register/Main.js
@@ -1,9 +1,86 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "@/styles/templates/Register/Main.module.css";
 import Breadcrumb from "@/components/modules/Breadcrumb/Breadcrumb";
 import AuthForm from "@/components/modules/AuthForm/AuthForm";
+import { useRouter } from "next/router";
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
+import useSWR from "swr";
+
+const fetcher = async () => {
+  const res = await fetch("/api/auth/me");
+
+  if (res.status !== 200) {
+    return false;
+  }
+
+  return true;
+};
 
 function Main() {
+  const router = useRouter();
+  const { data } = useSWR("GetMeLogin", fetcher);
+  const swal = withReactContent(Swal);
+
+  useEffect(() => {
+    if (data) {
+      router.replace("/my-account");
+    }
+  }, [data]);
+
+  const registerHandler = async (values, Swal) => {
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: values.username,
+        email: values.email,
+        password: values.password,
+      }),
+    });
+
+    Swal.close();
+
+    if (res.status === 400) {
+      swal.fire({
+        title: "اطلاعات وارد شده اشتباه می باشد !!",
+        icon: "error",
+        confirmButtonText: "باشه",
+      });
+    } else if (res.status === 422) {
+      swal.fire({
+        title: "کاربری قبلا با این ایمیل یا نام کاربری ثبت نام کرده !!",
+        icon: "error",
+        confirmButtonText: "باشه",
+      });
+    } else if (res.status === 201) {
+      swal
+        .fire({
+          title: "خوش آمدید",
+          icon: "success",
+          confirmButtonText: "رفتن به حساب کاربری",
+        })
+        .then(() => {
+          router.replace("/my-account");
+        });
+    }
+  };
+
+  const onSubmit = async (values) => {
+    swal.fire({
+      title: "لطفا چند لحظه صبر کنید",
+      timerProgressBar: true,
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading();
+
+        registerHandler(values, Swal);
+      },
+    });
+  };
+
   return (
     <>
       <Breadcrumb
@@ -21,7 +98,7 @@ function Main() {
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-md-8 col-lg-6">
-              <AuthForm />
+              <AuthForm onSubmit={onSubmit} />
             </div>
           </div>
         </div>
