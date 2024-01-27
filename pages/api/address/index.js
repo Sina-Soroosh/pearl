4d18f2098a3a49b063daf5b26a1db1132f95f201
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-27 17:30:45 +0330

    Coding the address api for the put method

--- pages/api/address/index.js
+++ pages/api/address/index.js
@@ -1,6 +1,7 @@
 import { connectToDB } from "@/config/db";
 import addressModel from "@/models/address";
 import { getMe } from "@/utils/myAccount";
+import addressCheck from "@/validators/address";
 
 const address = async (req, res) => {
   connectToDB();
@@ -12,13 +13,47 @@ const address = async (req, res) => {
       return res.status(401).json({ message: "You are unauthorized" });
     }
 
+    const mainAddress = await addressModel.findOne({ user: user._id });
+
     switch (req.method) {
       case "GET": {
-        const mainAddress = await addressModel.findOne({ user: user._id });
-
         return res.json(mainAddress);
       }
       case "PUT": {
+        const {
+          firstName = mainAddress.firstName,
+          lastName = mainAddress.lastName,
+          province = mainAddress.province,
+          city = mainAddress.city,
+          address = mainAddress.address,
+          postalCode = mainAddress.postalCode,
+          phone = mainAddress.phone,
+          email = mainAddress.email,
+        } = req.body;
+
+        const addressInfo = {
+          firstName,
+          lastName,
+          province,
+          city,
+          address,
+          postalCode,
+          phone,
+          email,
+        };
+
+        const isValidAddressInfo = addressCheck(addressInfo);
+
+        if (isValidAddressInfo !== true) {
+          return res.status(400).json({ message: "Parameters is not valid" });
+        }
+
+        await addressModel.findOneAndUpdate(
+          { _id: mainAddress._id },
+          addressInfo
+        );
+
+        return res.json({ message: "Update address successfully :))" });
       }
       default:
         return res.status(405).json({ message: "The method is not valid" });
