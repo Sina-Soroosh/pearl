b484dfd1e238606528417faf4547e267368c3291
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-01-29 11:58:51 +0330

    Coding product model

---
+++ models/product.js
@@ -0,0 +1,73 @@
+const { default: mongoose } = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      minLength: 3,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    shortName: {
+      type: String,
+      minLength: 3,
+      required: true,
+      index: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      minLength: 3,
+      required: true,
+    },
+    price: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+      default: 1,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      min: 0,
+      max: 100,
+      required: true,
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+    category: {
+      type: mongoose.Types.ObjectId,
+      ref: "category",
+      required: true,
+    },
+    infos: [
+      {
+        title: {
+          type: String,
+          required: true,
+        },
+        value: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const productModel =
+  mongoose.models.product || mongoose.model("product", productSchema);
+
+export default productModel;
