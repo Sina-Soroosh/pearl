a99d02db0b7e94934e89fc23af67f5535e183b90
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-02-03 17:44:39 +0330

    Coding the comments api for the post method

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,6 +1,8 @@
 import { connectToDB } from "@/config/db";
 import commentModel from "@/models/comment";
+import productModel from "@/models/product";
 import { getMe } from "@/utils/myAccount";
+import commentCheck from "@/validators/comment";
 
 const comments = async (req, res) => {
   connectToDB();
@@ -19,6 +21,41 @@ const comments = async (req, res) => {
         const commentsData = await commentModel.find();
 
         return res.json(commentsData);
+      }
+      case "POST": {
+        if (user === false) {
+          return res.status(401).json({ message: "You are unauthorize !!" });
+        }
+
+        const { body, star, product: shortNameProduct } = req.body;
+
+        const product = await productModel.findOne({
+          shortName: shortNameProduct,
+        });
+
+        if (!product) {
+          return res.status(404).json({ message: "Notfound product !!" });
+        }
+
+        const commentInfo = {
+          body,
+          star,
+          product: product._id.toString(),
+          creator: user._id.toString(),
+          isShow: false,
+        };
+
+        const isValidCommentInfo = await commentCheck(commentInfo);
+
+        if (isValidCommentInfo !== true) {
+          return res.status(400).json({ message: "Parameters is not valid" });
+        }
+
+        await commentModel.create(commentInfo);
+
+        return res
+          .status(201)
+          .json({ message: "Create comment successfully :))" });
       }
       default:
         return res.status(405).json({ message: "The method is not valid" });
