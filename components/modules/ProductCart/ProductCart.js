f528132aebe557c8af8db6fc2771550c58d0803e
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-02-08 16:24:38 +0330

    Connecting productCart module with the backend

--- components/modules/ProductCart/ProductCart.js
+++ components/modules/ProductCart/ProductCart.js
@@ -1,8 +1,79 @@
 import React from "react";
 import styles from "@/styles/modules/ProductCart/ProductCart.module.css";
 import Link from "next/link";
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
+import { useRouter } from "next/router";
 
 function ProductCart(props) {
+  const router = useRouter();
+  const swal = withReactContent(Swal);
+
+  const addToCartHandler = async (Swal) => {
+    const res = await fetch("/api/cart/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ product: props.shortName }),
+    });
+
+    Swal.close();
+
+    if (res.status === 401) {
+      swal
+        .fire({
+          title: "لطفا ابتدا وارد حساب کاربری خود شوید.",
+          icon: "error",
+          confirmButtonText: "باشه",
+        })
+        .then(() => {
+          router.push("/login");
+        });
+    } else if (res.status === 404 || res.status === 400) {
+      swal
+        .fire({
+          title: "ما این محصول را تموم کردیم.",
+          icon: "error",
+          confirmButtonText: "باشه",
+        })
+        .then(() => {
+          router.push("/login");
+        });
+    } else if (res.status === 201) {
+      swal.fire({
+        title: (
+          <p style={{ fontSize: "15px" }}>
+            محصول با موفقیت به سبد خرید شما اضافه شد
+          </p>
+        ),
+        toast: true,
+        position: "bottom-right",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+        icon: "success",
+      });
+    }
+  };
+
+  const onSubmit = () => {
+    swal.fire({
+      title: "لطفا چند لحظه صبر کنید",
+      timerProgressBar: true,
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading();
+
+        addToCartHandler(Swal);
+      },
+    });
+  };
+
   return (
     <>
       <div className="col-sm-6 col-lg-3">
@@ -15,7 +86,7 @@ function ProductCart(props) {
                 </Link>
               </div>
               {props.isAvailable ? (
-                <div className={styles.add_cart}>
+                <div className={styles.add_cart} onClick={onSubmit}>
                   <button>+ افزودن به سبد خرید</button>
                 </div>
               ) : null}
