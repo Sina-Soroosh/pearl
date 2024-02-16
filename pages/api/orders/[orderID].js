e80a7a2b3104b7718144ee00aa53a9d935741ae1
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-02-16 15:30:16 +0330

    Coding the orders/[orderID] api for the get method

---
+++ pages/api/orders/[orderID].js
@@ -0,0 +1,44 @@
+import { connectToDB } from "@/config/db";
+import orderModel from "@/models/order";
+import { getMe } from "@/utils/myAccount";
+import { isValidObjectId } from "mongoose";
+
+const order = async (req, res) => {
+  connectToDB();
+
+  const { orderID } = req.query;
+
+  const mainOrder = await orderModel.findOne({ orderID });
+
+  if (!mainOrder) {
+    return res.status(404).json({ message: "Notfound order !!" });
+  }
+
+  const user = await getMe(req.cookies);
+
+  if (
+    user === false ||
+    (user._id.toString() !== mainOrder.userID.toString() &&
+      user.role !== "ADMIN")
+  ) {
+    return res.status(403).json({ message: "You don't access to data !!" });
+  }
+
+  try {
+    switch (req.method) {
+      case "GET": {
+        return res.json(mainOrder);
+      }
+      case "DELETE": {
+      }
+      default:
+        return res.status(405).json({ message: "The method is not valid" });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Unknown internal server error !!", error });
+  }
+};
+
+export default order;
