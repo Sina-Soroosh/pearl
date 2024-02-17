fe29bcfab6456f1f4a6afa7d06c479eabde2f534
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-02-17 17:24:13 +0330

    Coding the faqs api for the post method

--- pages/api/faqs/index.js
+++ pages/api/faqs/index.js
@@ -1,5 +1,7 @@
 import { connectToDB } from "@/config/db";
 import faqModel from "@/models/faq";
+import { getMe } from "@/utils/myAccount";
+import faqCheck from "@/validators/faq";
 
 const faqs = async (req, res) => {
   connectToDB();
@@ -10,6 +12,31 @@ const faqs = async (req, res) => {
         const faqs = await faqModel.find();
 
         return res.json(faqs);
+      }
+      case "POST": {
+        const user = await getMe(req.cookies);
+
+        if (user === false || user.role !== "ADMIN") {
+          return res
+            .status(403)
+            .json({ message: "You don't access to data !!" });
+        }
+
+        const { title, body } = req.body;
+
+        const questionInfo = { title, body };
+
+        const isValidQuestion = faqCheck(questionInfo);
+
+        if (isValidQuestion !== true) {
+          return res.status(400).json({ message: "Parameters is not valid" });
+        }
+
+        await faqModel.create(questionInfo);
+
+        return res
+          .status(201)
+          .json({ message: "Create question successfully :))" });
       }
       default:
         return res.status(405).json({ message: "The method is not valid" });
