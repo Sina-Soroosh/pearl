b00f812993a55f5327386fe215f29a0f9c1c4b3b
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-02-17 17:58:29 +0330

    Coding the rules api for the post method

--- pages/api/rules/index.js
+++ pages/api/rules/index.js
@@ -1,5 +1,7 @@
 import { connectToDB } from "@/config/db";
 import ruleModel from "@/models/rule";
+import { getMe } from "@/utils/myAccount";
+import ruleCheck from "@/validators/rule";
 
 const rules = async (req, res) => {
   connectToDB();
@@ -12,6 +14,29 @@ const rules = async (req, res) => {
         return res.json(rules);
       }
       case "POST": {
+        const user = await getMe(req.cookies);
+
+        if (user === false || user.role !== "ADMIN") {
+          return res
+            .status(403)
+            .json({ message: "You don't access to data !!" });
+        }
+
+        const { title, body } = req.body;
+
+        const ruleInfo = { title, body };
+
+        const isValidRule = ruleCheck(ruleInfo);
+
+        if (isValidRule !== true) {
+          return res.status(400).json({ message: "Parameters is not valid" });
+        }
+
+        await ruleModel.create(ruleInfo);
+
+        return res
+          .status(201)
+          .json({ message: "Create rule successfully :))" });
       }
       default:
         return res.status(405).json({ message: "The method is not valid" });
