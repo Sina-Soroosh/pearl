1149789a0cd900504cbc20237ac1705c87d847d4
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-02-24 16:28:37 +0330

    Coding slider model

---
+++ models/slider.js
@@ -0,0 +1,32 @@
+const { default: mongoose } = require("mongoose");
+
+const sliderSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minLength: 3,
+    },
+    text: {
+      type: String,
+      minLength: 3,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    product: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const sliderModel =
+  mongoose.models.slider || mongoose.model("slider", sliderSchema);
+
+export default sliderModel;
