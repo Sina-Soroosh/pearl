379f661a845c7f61415b7b9226350cbb3fcb4569
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-03-01 18:22:30 +0330

    Coding Charts dashboard panel admin template

---
+++ components/templates/PanelAdmin/Dashboard/Charts/Charts.js
@@ -0,0 +1,68 @@
+import React from "react";
+import styles from "@/panelAdminStyles/Dashboard/Charts/Charts.module.css";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+} from "recharts";
+
+function Charts({ dataSeals, dataUsers }) {
+  return (
+    <div className={styles.charts}>
+      <div className={styles.content}>
+        <div className="row">
+          <div className="col-lg-6">
+            <div className={styles.chart}>
+              <div className={styles.title}>
+                <h3>مقدار فروش</h3>
+              </div>
+              <ResponsiveContainer>
+                <AreaChart data={dataSeals}>
+                  <XAxis dataKey="month" dy={5} />
+                  <YAxis dx={-40} />
+                  <Tooltip formatter={(value) => ["مقدار فروش", value]} />
+                  <Area
+                    type="monotone"
+                    dataKey="value"
+                    stroke="#eb7025"
+                    fill="#f9d4bd"
+                  />
+                </AreaChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className={styles.chart}>
+              <div className={styles.title}>
+                <h3>کاربران ثبت نام شده</h3>
+              </div>
+              <ResponsiveContainer>
+                <BarChart data={dataUsers}>
+                  <XAxis dataKey="month" dy={5} />
+                  <YAxis dx={-40} />
+                  <Tooltip
+                    formatter={(value) => ["کاربران ثبت نامی", value]}
+                    itemStyle={{ color: "#eb7025" }}
+                  />
+                  <Bar
+                    type="monotone"
+                    dataKey="value"
+                    stroke="#eb7025"
+                    fill="#f9d4bd"
+                  />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Charts;
--- pages/p-admin/index.js
+++ pages/p-admin/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Details from "@/panelAdminTemplates/Dashboard/Details/Details";
+import Charts from "@/panelAdminTemplates/Dashboard/Charts/Charts";
 
 function PanelAdmin() {
   return (
     <>
       <Details />
+      <Charts
+        dataSeals={[
+          { month: "May", value: 2100 },
+          { month: "Feb", value: 1200 },
+          { month: "July", value: 1800 },
+          { month: "Feb", value: 1900 },
+        ]}
+        dataUsers={[
+          { month: "May", value: 10 },
+          { month: "Feb", value: 20 },
+          { month: "July", value: 18 },
+          { month: "Feb", value: 9 },
+        ]}
+      />
     </>
   );
 }
