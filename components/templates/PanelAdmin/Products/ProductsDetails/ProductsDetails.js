e8339edf94847eb8817dd6401c0433124e631a8f
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-03-03 18:17:40 +0330

    Connecting p-admin/products page with the backend

--- components/templates/PanelAdmin/Products/ProductsDetails/ProductsDetails.js
+++ components/templates/PanelAdmin/Products/ProductsDetails/ProductsDetails.js
@@ -4,6 +4,8 @@ import styles from "@/panelAdminStyles/Products/ProductsDetails/ProductsDetails.
 import Link from "next/link";
 import { Add } from "@mui/icons-material";
 import { Button, TableCell, TableRow } from "@mui/material";
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
 
 const columns = [
   { id: 1, label: "" },
@@ -15,10 +17,197 @@ const columns = [
   { id: 8, label: "" },
 ];
 
-function ProductsDetails() {
+function ProductsDetails(props) {
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const [page, setPage] = useState(0);
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([...props.products]);
+  const swal = withReactContent(Swal);
+
+  const getProducts = async () => {
+    const res = await fetch(`/api/products`);
+
+    if (res.status === 200) {
+      const data = await res.json();
+
+      setProducts(data);
+    }
+  };
+
+  const removeHandler = async (values, Swal, shortName) => {
+    const res = await fetch(`/api/products/${shortName}`, {
+      method: "DELETE",
+    });
+
+    Swal.close();
+
+    switch (res.status) {
+      case 200:
+        swal.fire({
+          title: "محصول با موفقیت حذف شد",
+          confirmButtonText: "باشه",
+          icon: "success",
+        });
+
+        getProducts();
+        break;
+      default:
+        swal.fire({
+          title: "خطایی رخ داده. \n لطفا اتصال خود را چک کنید.",
+          confirmButtonText: "باشه",
+          icon: "error",
+        });
+        break;
+    }
+  };
+
+  const changeStatus = async (values, Swal, shortName) => {
+    const res = await fetch(`/api/products/changeStatus/${shortName}`, {
+      method: "PUT",
+    });
+
+    Swal.close();
+
+    switch (res.status) {
+      case 200:
+        swal.fire({
+          title: "وضعیت محصول با موفقیت تغییر کرد.",
+          confirmButtonText: "باشه",
+          icon: "success",
+        });
+
+        getProducts();
+        break;
+      default:
+        swal.fire({
+          title: "خطایی رخ داده. \n لطفا اتصال خود را چک کنید.",
+          confirmButtonText: "باشه",
+          icon: "error",
+        });
+        break;
+    }
+  };
+
+  const discountHandler = async (values, Swal, shortName) => {
+    const res = await fetch(`/api/products/${shortName}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ discount: values.discount }),
+    });
+
+    Swal.close();
+
+    switch (res.status) {
+      case 200:
+        swal.fire({
+          title: "تخفیف با موفقیت اعمال شد",
+          confirmButtonText: "باشه",
+          icon: "success",
+        });
+
+        getProducts();
+        break;
+      default:
+        swal.fire({
+          title: "خطایی رخ داده. \n لطفا اتصال خود را چک کنید.",
+          confirmButtonText: "باشه",
+          icon: "error",
+        });
+        break;
+    }
+  };
+
+  const loader = async (func, values, shortName) => {
+    swal.fire({
+      title: "لطفا چند لحظه صبر کنید",
+      timerProgressBar: true,
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading();
+
+        func(values, Swal, shortName);
+      },
+    });
+  };
+
+  const onSubmitChangeStatus = (product) => {
+    swal
+      .fire({
+        title: "آیا مطمئنید که میخواهید وضعیت این محصول را تغییر دهید؟",
+        icon: "question",
+        confirmButtonText: "بله",
+        cancelButtonText: "نه",
+        showCancelButton: true,
+        confirmButtonColor: "rgb(69 71 76)",
+        cancelButtonColor: "var(--orange)",
+        focusCancel: true,
+      })
+      .then((res) => {
+        if (res.isConfirmed) {
+          loader(changeStatus, {}, product.shortName);
+        }
+      });
+  };
+
+  const onSubmitDelete = (product) => {
+    swal
+      .fire({
+        title: "آیا مطمئنید که میخواهید این محصول را حذف کنید؟",
+        icon: "question",
+        confirmButtonText: "بله",
+        cancelButtonText: "نه",
+        showCancelButton: true,
+        confirmButtonColor: "rgb(69 71 76)",
+        cancelButtonColor: "var(--orange)",
+        focusCancel: true,
+      })
+      .then((res) => {
+        if (res.isConfirmed) {
+          loader(removeHandler, {}, product.shortName);
+        }
+      });
+  };
+
+  const onSubmitDiscount = (product) => {
+    let discount = product.discount;
+
+    swal
+      .fire({
+        title: "تخفیف جدید را وارد کنید",
+        html: (
+          <>
+            <div className={styles.form_update}>
+              <input
+                type="number"
+                id="title"
+                min={0}
+                max={100}
+                defaultValue={discount}
+                onChange={(e) => (discount = +e.target.value)}
+              />
+            </div>
+          </>
+        ),
+        confirmButtonText: "ثبت تخفیف",
+        confirmButtonColor: "#eb7025",
+      })
+      .then((res) => {
+        if (res.isConfirmed) {
+          if (discount < 0 || discount > 100) {
+            swal
+              .fire({
+                title: "لطفا یک مقدار درست وارد نمایید.",
+                icon: "error",
+                confirmButtonText: "باشه",
+              })
+              .then(() => onSubmitDiscount(product));
+          } else {
+            loader(discountHandler, { discount }, product.shortName);
+          }
+        }
+      });
+  };
 
   return (
     <>
@@ -56,32 +245,51 @@ function ProductsDetails() {
                     >
                       <TableCell className={styles.cell} align="right">
                         <img
-                          src="/images/products/520c7dbee04ab52bfd3f4441a.png"
-                          alt=""
+                          src={product.image}
+                          alt={product.title}
                           className={styles.image}
                         />
                       </TableCell>
                       <TableCell className={styles.cell} align="right">
-                        <Link href={`/products/chairs`}>صندلی</Link>
+                        <Link href={`/products/${product.shortName}`}>
+                          {product.title}
+                        </Link>
                       </TableCell>
                       <TableCell className={styles.cell} align="right">
-                        <Link href={`/products/chairs`}>chairs</Link>
+                        <Link href={`/products/${product.shortName}`}>
+                          {product.shortName}
+                        </Link>
                       </TableCell>
                       <TableCell className={styles.cell} align="right">
-                        130,000 تومان
+                        {product.price.toLocaleString()} تومان
                       </TableCell>
                       <TableCell className={styles.cell} align="right">
-                        30%
+                        {product.discount}%
                       </TableCell>
                       <TableCell className={styles.cell} align="right">
-                        موجود
+                        {product.isAvailable ? "موجود" : "ناموجود"}
                       </TableCell>
                       <TableCell className={styles.cell} align="right">
-                        <button className="btn btn-success">تغییر وضعیت</button>
-                        <button className="btn btn-warning">
+                        <button
+                          className={`btn ${
+                            product.isAvailable ? "btn-danger" : "btn-success"
+                          }`}
+                          onClick={() => onSubmitChangeStatus(product)}
+                        >
+                          تغییر وضعیت
+                        </button>
+                        <button
+                          className="btn btn-warning"
+                          onClick={() => onSubmitDiscount(product)}
+                        >
                           افزودن تخفیف
                         </button>
-                        <button className="btn btn-danger">حذف</button>
+                        <button
+                          className="btn btn-danger"
+                          onClick={() => onSubmitDelete(product)}
+                        >
+                          حذف
+                        </button>
                       </TableCell>
                     </TableRow>
                   );
--- pages/p-admin/products/index.js
+++ pages/p-admin/products/index.js
@@ -1,12 +1,35 @@
 import React from "react";
 import ProductsDetails from "@/panelAdminTemplates/Products/ProductsDetails/ProductsDetails";
+import { connectToDB } from "@/config/db";
+import { getMe } from "@/utils/myAccount";
+import productModel from "@/models/product";
 
-function Products() {
+function Products({ products }) {
   return (
     <>
-      <ProductsDetails />
+      <ProductsDetails products={products} />
     </>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  await connectToDB();
+
+  const user = await getMe(ctx.req.cookies);
+
+  if (user === false || user.role !== "ADMIN") {
+    return {
+      redirect: { destination: "/login" },
+    };
+  }
+
+  const products = await productModel.find();
+
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
+};
 
 export default Products;
