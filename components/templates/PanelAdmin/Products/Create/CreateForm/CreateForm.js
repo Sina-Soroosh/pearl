655664f7a0902f5c56aefe417a8be992ed83aa0e
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-03-04 17:18:59 +0330

    Coding CreateForm products/create panel admin template

---
+++ components/templates/PanelAdmin/Products/Create/CreateForm/CreateForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "@/panelAdminStyles/Products/Create/CreateForm/CreateForm.module.css";
+import Link from "next/link";
+
+function CreateForm() {
+  return (
+    <>
+      <div className={styles.create_form}>
+        <div className={styles.content}>
+          <div className={styles.top_form}>
+            <div className={styles.title}>
+              <h3>افزودن محصول</h3>
+            </div>
+            <div className={styles.create_product}>
+              <Link href="/p-admin/products/" className="btn btn-primary">
+                لیست محصولات
+              </Link>
+            </div>
+          </div>
+          <div className={styles.form}></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CreateForm;
---
+++ pages/p-admin/products/create/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateForm from "@/panelAdminTemplates/Products/Create/CreateForm/CreateForm";
+
+function Create() {
+  return (
+    <>
+      <CreateForm />
+    </>
+  );
+}
+
+export default Create;
