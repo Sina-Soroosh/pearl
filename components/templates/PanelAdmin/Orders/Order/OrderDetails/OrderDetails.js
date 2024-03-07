9ebf14ae343fdca5e86e14b6fc882040cae51cea
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-03-07 10:19:32 +0330

    Connecting p-admin/orders/[orderID] page with the backend

--- components/templates/PanelAdmin/Orders/Order/OrderDetails/OrderDetails.js
+++ components/templates/PanelAdmin/Orders/Order/OrderDetails/OrderDetails.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styles from "@/panelAdminStyles/Orders/Order/OrderDetails/OrderDetails.module.css";
 import TableDetails from "@/panelAdminModules/TableDetails/TableDetails";
-import { Rating, TableCell, TableRow } from "@mui/material";
+import { TableCell, TableRow } from "@mui/material";
 import Swal from "sweetalert2";
+import { useRouter } from "next/router";
 
 const columnsProducts = [
   { id: 1, label: "محصول" },
@@ -13,8 +14,110 @@ const columnsDetails = [
   { id: 1, label: "عنوان" },
   { id: 2, label: "مقدار" },
 ];
+function OrderDetails({ order }) {
+  const router = useRouter();
 
-function OrderDetails() {
+  const changeStatus = async (swal) => {
+    const res = await fetch(`/api/orders/changeStatus/${order.orderID}`, {
+      method: "PUT",
+    });
+
+    Swal.close();
+
+    switch (res.status) {
+      case 200:
+        swal.fire({
+          title: "وضعیت سفارش با موفقیت تغییر کرد.",
+          confirmButtonText: "باشه",
+          icon: "success",
+        });
+
+        router.push("/p-admin/orders");
+        break;
+      default:
+        swal.fire({
+          title: "خطایی رخ داده. \n لطفا اتصال خود را چک کنید.",
+          confirmButtonText: "باشه",
+          icon: "error",
+        });
+        break;
+    }
+  };
+
+  const removeOrder = async (swal) => {
+    const res = await fetch(`/api/orders/${order.orderID}`, {
+      method: "DELETE",
+    });
+
+    Swal.close();
+
+    switch (res.status) {
+      case 200:
+        swal.fire({
+          title: "سفارش با موفقیت حذف شد",
+          confirmButtonText: "باشه",
+          icon: "success",
+        });
+
+        router.replace("/p-admin/orders");
+        break;
+      default:
+        swal.fire({
+          title: "خطایی رخ داده. \n لطفا اتصال خود را چک کنید.",
+          confirmButtonText: "باشه",
+          icon: "error",
+        });
+        break;
+    }
+  };
+
+  const loader = async (func, ID) => {
+    Swal.fire({
+      title: "لطفا چند لحظه صبر کنید",
+      timerProgressBar: true,
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading();
+
+        func(Swal);
+      },
+    });
+  };
+
+  const onSubmitChangeStatus = (ID) => {
+    Swal.fire({
+      title: "آیا مطمئنید که میخواهید وضعیت این سفارش را تغییر دهید؟",
+      icon: "question",
+      confirmButtonText: "بله",
+      cancelButtonText: "نه",
+      showCancelButton: true,
+      confirmButtonColor: "rgb(69 71 76)",
+      cancelButtonColor: "var(--orange)",
+      focusCancel: true,
+    }).then((res) => {
+      if (res.isConfirmed) {
+        loader(changeStatus);
+      }
+    });
+  };
+
+  const onSubmitRemove = () => {
+    Swal.fire({
+      title: "آیا مطمئنید که میخواهید این سفارش را حذف کنید؟",
+      icon: "question",
+      confirmButtonText: "بله",
+      cancelButtonText: "نه",
+      showCancelButton: true,
+      confirmButtonColor: "rgb(69 71 76)",
+      cancelButtonColor: "var(--orange)",
+      focusCancel: true,
+    }).then((res) => {
+      if (res.isConfirmed) {
+        loader(removeOrder);
+      }
+    });
+  };
+
   return (
     <>
       <div className={styles.details}>
@@ -24,49 +127,53 @@ function OrderDetails() {
               <h3>جزییات سفارش</h3>
             </div>
             <div className={styles.buttons}>
-              <button className="btn btn-primary fs-5">تغییر وضعیت</button>
-              <button className="btn btn-danger fs-5">حذف سفارش</button>
+              <button
+                className="btn btn-primary fs-5"
+                onClick={onSubmitChangeStatus}
+              >
+                تغییر وضعیت
+              </button>
+              <button className="btn btn-danger fs-5" onClick={onSubmitRemove}>
+                حذف سفارش
+              </button>
             </div>
           </div>
           <div className={styles.text}>
             <p>
-              سفارش #<span>10005</span> در تاریخ <span>2024/02/12</span> ثبت شده
-              است و در حال حاضر در وضعیت در <span>درحال بررسی</span> می‌باشد.
+              سفارش #<span>{order.orderID}</span> در تاریخ{" "}
+              <span>
+                {" "}
+                {order.createdAt.slice(0, 4)}/{order.createdAt.slice(5, 7)}/
+                {order.createdAt.slice(8, 10)}
+              </span>{" "}
+              ثبت شده است و در حال حاضر در وضعیت در{" "}
+              <span>
+                {(order.status === "pending" && "در حال بررسی") ||
+                  (order.status === "shipped" && "در حال ارسال") ||
+                  (order.status === "delivered" && "تحویل داده شده")}
+              </span>{" "}
+              می‌باشد.
             </p>
           </div>
 
           <div className={styles.table}>
             <TableDetails columns={columnsProducts} isPagination={false}>
-              <TableRow hover role="checkbox" tabIndex={-1}>
-                <TableCell align="right" style={{ fontSize: "15px" }}>
-                  میز × 2
-                </TableCell>
-                <TableCell align="right" style={{ fontSize: "15px" }}>
-                  2,000,000 تومان
-                </TableCell>
-              </TableRow>
-              <TableRow hover role="checkbox" tabIndex={-1}>
-                <TableCell align="right" style={{ fontSize: "15px" }}>
-                  میز × 2
-                </TableCell>
-                <TableCell align="right" style={{ fontSize: "15px" }}>
-                  2,000,000 تومان
-                </TableCell>
-              </TableRow>
-              <TableRow hover role="checkbox" tabIndex={-1}>
-                <TableCell align="right" style={{ fontSize: "15px" }}>
-                  میز × 2
-                </TableCell>
-                <TableCell align="right" style={{ fontSize: "15px" }}>
-                  2,000,000 تومان
-                </TableCell>
-              </TableRow>
+              {order.products.map((product) => (
+                <TableRow hover role="checkbox" tabIndex={-1} key={product._id}>
+                  <TableCell align="right" style={{ fontSize: "15px" }}>
+                    {product.title} × {product.count}
+                  </TableCell>
+                  <TableCell align="right" style={{ fontSize: "15px" }}>
+                    {(product.price * product.count).toLocaleString()} تومان
+                  </TableCell>
+                </TableRow>
+              ))}
               <TableRow hover role="checkbox" tabIndex={-1}>
                 <TableCell align="right" style={{ fontSize: "15px" }}>
                   قیمت نهایی
                 </TableCell>
                 <TableCell align="right" style={{ fontSize: "15px" }}>
-                  6,000,000 تومان
+                  {order.total.toLocaleString()} تومان
                 </TableCell>
               </TableRow>
             </TableDetails>
@@ -78,7 +185,7 @@ function OrderDetails() {
                   نام گیرنده
                 </TableCell>
                 <TableCell align="right" style={{ fontSize: "15px" }}>
-                  سینا سروش فر
+                  {order.user.firstName} {order.user.lastName}
                 </TableCell>
               </TableRow>
               <TableRow hover role="checkbox" tabIndex={-1}>
@@ -86,7 +193,7 @@ function OrderDetails() {
                   ایمیل
                 </TableCell>
                 <TableCell align="right" style={{ fontSize: "15px" }}>
-                  <a href={`mailto:`}>sina@gmail.com</a>
+                  <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
                 </TableCell>
               </TableRow>
               <TableRow hover role="checkbox" tabIndex={-1}>
@@ -94,7 +201,7 @@ function OrderDetails() {
                   شماره تلفن
                 </TableCell>
                 <TableCell align="right" style={{ fontSize: "15px" }}>
-                  <a href={`tel:`}>09176009832</a>
+                  <a href={`tel:${order.user.phone}`}>{order.user.phone}</a>
                 </TableCell>
               </TableRow>
               <TableRow hover role="checkbox" tabIndex={-1}>
@@ -102,7 +209,7 @@ function OrderDetails() {
                   استان
                 </TableCell>
                 <TableCell align="right" style={{ fontSize: "15px" }}>
-                  فارس
+                  {order.address.province}
                 </TableCell>
               </TableRow>
               <TableRow hover role="checkbox" tabIndex={-1}>
@@ -110,7 +217,7 @@ function OrderDetails() {
                   شهر
                 </TableCell>
                 <TableCell align="right" style={{ fontSize: "15px" }}>
-                  جهرم
+                  {order.address.city}
                 </TableCell>
               </TableRow>
               <TableRow hover role="checkbox" tabIndex={-1}>
@@ -118,7 +225,7 @@ function OrderDetails() {
                   ادرس
                 </TableCell>
                 <TableCell align="right" style={{ fontSize: "15px" }}>
-                  خیابان عبرت
+                  {order.address.address}
                 </TableCell>
               </TableRow>
               <TableRow hover role="checkbox" tabIndex={-1}>
@@ -126,7 +233,7 @@ function OrderDetails() {
                   کدپستی
                 </TableCell>
                 <TableCell align="right" style={{ fontSize: "15px" }}>
-                  0732434838
+                  {order.address.postalCode}
                 </TableCell>
               </TableRow>
               <TableRow hover role="checkbox" tabIndex={-1}>
@@ -134,7 +241,7 @@ function OrderDetails() {
                   توضیحات
                 </TableCell>
                 <TableCell align="right" style={{ fontSize: "15px" }}>
-                  توسیبتیبا
+                  {order.desc}
                 </TableCell>
               </TableRow>
             </TableDetails>
--- pages/p-admin/orders/[orderID]/index.js
+++ pages/p-admin/orders/[orderID]/index.js
@@ -1,12 +1,47 @@
 import React from "react";
 import OrderDetails from "@/panelAdminTemplates/Orders/Order/OrderDetails/OrderDetails";
+import { connectToDB } from "@/config/db";
+import { getMe } from "@/utils/myAccount";
+import orderModel from "@/models/order";
+import Head from "next/head";
 
-function Order() {
+function Order({ order }) {
   return (
     <>
-      <OrderDetails />
+      <Head>
+        <title>پنل مدیریت - جزییات سفارش</title>
+      </Head>
+
+      <OrderDetails order={order} />
     </>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  await connectToDB();
+
+  const { orderID } = ctx.query;
+  const user = await getMe(ctx.req.cookies);
+
+  if (user === false || user.role !== "ADMIN") {
+    return {
+      redirect: { destination: "/login" },
+    };
+  }
+
+  const order = await orderModel.findOne({ orderID });
+
+  if (!order) {
+    return {
+      redirect: { destination: "/p-admin/orders" },
+    };
+  }
+
+  return {
+    props: {
+      order: JSON.parse(JSON.stringify(order)),
+    },
+  };
+};
 
 export default Order;
