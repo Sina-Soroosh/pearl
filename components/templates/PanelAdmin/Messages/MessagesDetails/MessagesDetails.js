eac5fda9b3417844534682da1ef7c8bec586c3c9
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-03-08 09:28:08 +0330

    Connecting p-admin/messages page with the backend

--- components/templates/PanelAdmin/Messages/MessagesDetails/MessagesDetails.js
+++ components/templates/PanelAdmin/Messages/MessagesDetails/MessagesDetails.js
@@ -3,7 +3,6 @@ import styles from "@/panelAdminStyles/Messages/MessagesDetails/MessagesDetails.
 import TableDetails from "@/panelAdminModules/TableDetails/TableDetails";
 import { Rating, TableCell, TableRow } from "@mui/material";
 import Swal from "sweetalert2";
-import Link from "next/link";
 
 const columns = [
   { id: 1, label: "نام" },
@@ -13,10 +12,84 @@ const columns = [
   { id: 5, label: "" },
 ];
 
-function MessagesDetails() {
+function MessagesDetails(props) {
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const [page, setPage] = useState(0);
-  const [messages, setMessages] = useState([{ _id: 1 }]);
+  const [messages, setMessages] = useState([...props.messages]);
+
+  const getMessages = async () => {
+    const res = await fetch("/api/messages");
+
+    if (res.status === 200) {
+      const data = await res.json();
+
+      setMessages(data.reverse());
+    }
+  };
+
+  const showMessageHandler = (text) => {
+    Swal.fire({
+      title: text,
+      confirmButtonText: "پیام را دیدم",
+    });
+  };
+
+  const removeMessage = async (swal, ID) => {
+    const res = await fetch(`/api/messages/${ID}`, {
+      method: "DELETE",
+    });
+
+    Swal.close();
+
+    switch (res.status) {
+      case 200:
+        swal.fire({
+          title: "پیام با موفقیت حذف شد",
+          confirmButtonText: "باشه",
+          icon: "success",
+        });
+
+        getMessages();
+        break;
+      default:
+        swal.fire({
+          title: "خطایی رخ داده. \n لطفا اتصال خود را چک کنید.",
+          confirmButtonText: "باشه",
+          icon: "error",
+        });
+        break;
+    }
+  };
+
+  const loader = async (func, ID) => {
+    Swal.fire({
+      title: "لطفا چند لحظه صبر کنید",
+      timerProgressBar: true,
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading();
+
+        func(Swal, ID);
+      },
+    });
+  };
+
+  const onSubmitRemove = (ID) => {
+    Swal.fire({
+      title: "آیا مطمئنید که میخواهید این پیام را حذف کنید؟",
+      icon: "question",
+      confirmButtonText: "بله",
+      cancelButtonText: "نه",
+      showCancelButton: true,
+      confirmButtonColor: "rgb(69 71 76)",
+      cancelButtonColor: "var(--orange)",
+      focusCancel: true,
+    }).then((res) => {
+      if (res.isConfirmed) {
+        loader(removeMessage, ID);
+      }
+    });
+  };
 
   return (
     <>
@@ -46,21 +119,29 @@ function MessagesDetails() {
                       tabIndex={-1}
                     >
                       <TableCell style={{ fontSize: "15px" }} align="right">
-                        سینا سروش فر
+                        {message.name}
                       </TableCell>
                       <TableCell style={{ fontSize: "15px" }} align="right">
-                        <a href={`mailto:`}>sina@gmail.com</a>
+                        <a href={`mailto:${message.email}`}>{message.email}</a>
                       </TableCell>
                       <TableCell style={{ fontSize: "15px" }} align="right">
-                        <button className="btn btn-primary fs-5 mx-3">
+                        <button
+                          className="btn btn-primary fs-5 mx-3"
+                          onClick={() => showMessageHandler(message.message)}
+                        >
                           دیدن پیام
                         </button>
                       </TableCell>
                       <TableCell style={{ fontSize: "15px" }} align="right">
-                        2024/03/02
+                        {message.createdAt.slice(0, 4)}/
+                        {message.createdAt.slice(5, 7)}/
+                        {message.createdAt.slice(8, 10)}
                       </TableCell>
                       <TableCell style={{ fontSize: "15px" }} align="right">
-                        <button className="btn btn-danger fs-5 mx-3">
+                        <button
+                          className="btn btn-danger fs-5 mx-3"
+                          onClick={() => onSubmitRemove(message._id)}
+                        >
                           حذف پیام
                         </button>
                       </TableCell>
--- pages/p-admin/messages/index.js
+++ pages/p-admin/messages/index.js
@@ -1,12 +1,40 @@
 import React from "react";
 import MessagesDetails from "@/panelAdminTemplates/Messages/MessagesDetails/MessagesDetails";
+import Head from "next/head";
+import { connectToDB } from "@/config/db";
+import { getMe } from "@/utils/myAccount";
+import messageModel from "@/models/message";
 
-function Messages() {
+function Messages({ messages }) {
   return (
     <>
-      <MessagesDetails />
+      <Head>
+        <title>پنل مدیریت - پیام ها</title>
+      </Head>
+
+      <MessagesDetails messages={messages} />
     </>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  await connectToDB();
+
+  const user = await getMe(ctx.req.cookies);
+
+  if (user === false || user.role !== "ADMIN") {
+    return {
+      redirect: { destination: "/login" },
+    };
+  }
+
+  const messages = await messageModel.find();
+
+  return {
+    props: {
+      messages: JSON.parse(JSON.stringify(messages.reverse())),
+    },
+  };
+};
 
 export default Messages;
