552bacb10477ba619cda6b3e9bf0bcee8adfe224
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-03-08 10:24:48 +0330

    Coding CreateQuestion questions panel admin template

---
+++ pages/p-admin/questions/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateQuestion from "@/panelAdminTemplates/Questions/CreateQuestion/CreateQuestion";
+
+function Questions() {
+  return (
+    <>
+      <CreateQuestion />
+    </>
+  );
+}
+
+export default Questions;
